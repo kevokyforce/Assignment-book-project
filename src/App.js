6124e375c1d21bca3fc888906a1b758f8947771e
kevokyforce <kevokyforce@gmail.com>
2024-06-07 16:25:24 +0300

    Book Node Js & React Advanced Assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import { ApolloProvider } from '@apollo/client';
+import client from './apollo';
+import BookReader from './BookReader';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="app">
+        <h1>Book Reader</h1>
+        <BookReader />
+      </div>
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
